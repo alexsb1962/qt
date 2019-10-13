1920b964059e494fe08928cf2d58c5d3a8da3a89
alex <alexsb@mail.ru>
2019-10-14 00:54:57 +0300

    'Работает

--- qt.py
+++ qt.py
@@ -3,6 +3,7 @@ from PyQt5 import QtWidgets
 import form
 import sys
 
+
 class App(QtWidgets.QMainWindow, form.Ui_MainWindow):
     def __init__(self):
         # Это здесь нужно для доступа к переменным, методам
@@ -24,21 +25,19 @@ def StartGUI():
 import sys, random
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QColor, QPen
-from PyQt5.QtCore import Qt,QRect,QThread
+from PyQt5.QtCore import Qt, QRect, QThread
 import time
 
 
-
 class common():
+    rect_on = False
 
-    rect_on=False
-
-    mainrect=QRect()
+    mainrect = QRect()
     mainrect.setRect(300, 300, 800, 450)
 
-    rect=QRect()
-    rect.setRect(50,50,400,250)
-    radius=30
+    rect = QRect()
+    rect.setRect(50, 50, 400, 250)
+    radius = 30
 
 
 class Example(QWidget):
@@ -59,36 +58,46 @@ class Example(QWidget):
         qp.end()
 
     def SomeDraw(self, qp):
-        print('SomeDraw')
         qp.setPen(Qt.red)
         size = self.size()
 
         if common.rect_on:
             qp.drawRect(common.rect)
         else:
-            qp.drawRoundedRect(common.rect,common.radius,common.radius)
+            qp.drawRoundedRect(common.rect, common.radius, common.radius)
 
         for i in range(1000):
             x = random.randint(1, size.width() - 1)
             y = random.randint(1, size.height() - 1)
             qp.drawPoint(x, y)
 
+
 class Worker(QThread):
+    wid = None
+
+    def __init__(self, Widget=None):
+        super().__init__()
+        self.wid = Widget
+
     def run(self):
-        #здесь то и исполняется основной код
-        for i in range(20):
-            time.sleep(0.5)
-            common.rect_on=not common.rect_on
-            time.sleep(0.5)
+        # здесь то и исполняется основной код
+        st = time.time()
+        while (True):
+            common.rect_on = not common.rect_on
+            self.wid.update()
+            time.sleep(0.1)
+            if (time.time() - st) > 5:
+                break
+
         print('End of circle')
 
 
-
 if __name__ == '__main__':
-
-    w=Worker()
-    w.start()
-
     app = QApplication(sys.argv)
     ex = Example()
+
+    w = Worker(ex)
+
+    w.start()
+
     sys.exit(app.exec_())
